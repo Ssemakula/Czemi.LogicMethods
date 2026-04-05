e2da3820a77f069118a61d8e9cd1e8169199ebd1
Ssemakula <ss.eessess@hotmail.com>
2026-04-05 09:58:41 +0300

    Added nullable types to IsTrue()

--- Czemi.LogicMethods/IsTrue.cs
+++ Czemi.LogicMethods/IsTrue.cs
@@ -29,6 +29,27 @@ namespace Czemi.LogicMethods
 
         public static bool IsTrue(this decimal value) => value != 0m;
 
+        public static bool IsTrue(this int? value) => value != 0;
+
+        public static bool IsTrue(this uint? value) => value != null && value != 0U;
+
+        public static bool IsTrue(this long? value) => value != null && value != 0L;
+        public static bool IsTrue(this ulong? value) => value != null && value != 0UL;
+
+        public static bool IsTrue(this short? value) => value != null && value != 0;
+
+        public static bool IsTrue(this ushort? value) => value != null && value != 0;
+        public static bool IsTrue(this BigInteger? value) => value != null && value != 0;
+
+        public static bool IsTrue(this byte? value) => value != null && value != 0;
+
+        public static bool IsTrue(this sbyte? value) => value != null && value != 0;
+
+        public static bool IsTrue(this float? value) => value != null && value != 0f;
+
+        public static bool IsTrue(this double? value) => value != null && value != 0d;
+        public static bool IsTrue(this decimal? value) => value != null && value != 0m;
+
         public static bool IsTrue(this Enum value) => Convert.ToInt64(value) != 0;
 
         public static bool IsTrue(this string value) => !string.IsNullOrEmpty(value);
